018a818ae99d0dd83f0e9ec4fb818c774b7d908a
svfabio <svfabio.andres@gmail.com>
2025-08-09 23:45:37 -0400

    add&Connect_BD-MySQL

---
+++ src/test/java/com/restaurante/pos/PosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
